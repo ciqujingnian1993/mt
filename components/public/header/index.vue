92f84ce4dc9e4fd56fb80c67bf3d329b9ecc6c33
yb_li <1429633055@qq.com>
2019-05-04 17:01:52 +0800

    init commit

---
+++ components/public/header/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-row :gutter="0" class="m-header">
+    <el-col :span="4">
+      <geo/>
+    </el-col>
+    <el-col :span="6">
+      <user/>
+    </el-col>
+    <el-col :span="14"><div class="grid-content bg-purple"></div></el-col>
+  </el-row>
+</template>
+
+<script>
+  import Geo from './geo.vue'
+  import User from './user.vue'
+    export default {
+      components:{
+        Geo,
+        User
+      }
+    }
+</script>
+
+<style lang="scss">
+  @import "../../../assets/css/public/layout.scss";
+</style>
